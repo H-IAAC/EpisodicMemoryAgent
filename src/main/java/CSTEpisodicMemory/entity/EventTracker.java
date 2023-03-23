683208db710fd3bb467b7a4c1971ffd1d758818b
Bruno Silva <bruno_gsilva@outlook.com>
2023-03-23 16:42:01 -0300

    Added abstract class EventTracker to encapsulate general behaviour of tracking an event

---
+++ src/main/java/CSTEpisodicMemory/entity/EventTracker.java
@@ -0,0 +1,171 @@
+package CSTEpisodicMemory.entity;
+
+import br.unicamp.cst.core.entities.Codelet;
+import br.unicamp.cst.core.entities.Memory;
+import br.unicamp.cst.core.entities.MemoryObject;
+import br.unicamp.cst.representation.idea.Idea;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static CSTEpisodicMemory.util.IdeaPrinter.fullPrint;
+
+public abstract class EventTracker extends Codelet {
+
+    private String inputMemoryName = "PERCEPTION_MEMORY";
+    private String outputMemoryName = "EVENTS_MEMORY";
+    private Memory perceptionInputMO;
+    private Memory eventsOutputMO;
+
+    private int bufferSize = 1;
+    private int bufferStepSize = 1;
+    private List<Idea> inputIdeaBuffer = new LinkedList<Idea>();
+    private Idea initialEventIdea;
+    private Idea currentInputIdea;
+    private String eventCategoryName = null;
+    private int count = 1;
+
+    public EventTracker(String inputMemoryName, String outputMemoryName) {
+        this.inputMemoryName = inputMemoryName;
+        this.outputMemoryName = outputMemoryName;
+    }
+
+    @Override
+    public void accessMemoryObjects() {
+        this.perceptionInputMO=(MemoryObject)this.getInput(this.getInputMemoryName());
+        this.currentInputIdea = (Idea) perceptionInputMO.getI();
+        this.eventsOutputMO=(MemoryObject)this.getOutput(this.getOutputMemoryName());
+    }
+
+    @Override
+    public void calculateActivation() {
+
+    }
+
+    @Override
+    public void proc() {
+        //Initialize event track buffer
+        if (inputIdeaBuffer.size() == 0) {
+            inputIdeaBuffer.add(currentInputIdea.clone());
+        } else {
+            if (inputIdeaBuffer.size() < this.bufferSize) {
+                if (((int) currentInputIdea.get("Step").getValue())
+                        - ((int) inputIdeaBuffer.get(inputIdeaBuffer.size() - 1).get("Step").getValue())
+                        >= bufferStepSize) {
+                    inputIdeaBuffer.add(currentInputIdea.clone());
+                }
+            } else {
+                if (((int) currentInputIdea.get("Step").getValue())
+                        - ((int) inputIdeaBuffer.get(inputIdeaBuffer.size() - 1).get("Step").getValue())
+                        >= bufferStepSize) {
+                    //Check if current state is coherent with previous states and event category
+                    boolean check = belongsToEvent(inputIdeaBuffer, currentInputIdea);
+                    if (check) {
+                        Idea drop = inputIdeaBuffer.remove(0);
+                        //Copies start of the event
+                        if (initialEventIdea == null) this.initialEventIdea = drop.clone();
+                        inputIdeaBuffer.add(currentInputIdea.clone());
+                    } else {
+                        if (initialEventIdea != null) {
+                            Idea event = constructEventIdea(inputIdeaBuffer.get(1));
+                            inputIdeaBuffer.clear();
+                            inputIdeaBuffer.add(currentInputIdea.clone());
+                            initialEventIdea = null;
+                            Idea eventsIdea = (Idea) eventsOutputMO.getI();
+                            eventsIdea.add(event);
+                            System.out.println(fullPrint(eventsIdea));
+                        } else {
+                            inputIdeaBuffer.remove(0);
+                            inputIdeaBuffer.add(currentInputIdea.clone());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private Idea constructEventIdea(Idea lastEventIdea) {
+        Idea eventIdea = new Idea("Event" + count++, this.eventCategoryName, "Episode", 1);
+        Idea time1 = new Idea("", 1, "TimeStep", 1);
+        Idea time2 = new Idea("", 2, "TimeStep", 1);
+            time1.add(extractRelevant(this.initialEventIdea));
+            time2.add(extractRelevant(lastEventIdea));
+            eventIdea.add(time1);
+            eventIdea.add(time2);
+            return eventIdea;
+    }
+
+    public abstract boolean belongsToEvent(List<Idea> inputIdeaBuffer, Idea currentInputIdea);
+
+    public abstract Idea extractRelevant(Idea idea);
+
+    public int getBufferSize() {
+        return bufferSize;
+    }
+
+    public void setBufferSize(int bufferSize) {
+        if (bufferSize > 0)
+            this.bufferSize = bufferSize;
+    }
+
+    public int getBufferStepSize() {
+        return bufferStepSize;
+    }
+
+    public void setBufferStepSize(int bufferStepSize) {
+        if (bufferStepSize > 0)
+            this.bufferStepSize = bufferStepSize;
+    }
+
+    public List<Idea> getInputIdeaBuffer() {
+        return inputIdeaBuffer;
+    }
+
+    public void setInputIdeaBuffer(List<Idea> inputIdeaBuffer) {
+        this.inputIdeaBuffer = inputIdeaBuffer;
+    }
+
+    public Idea getInitialEventIdea() {
+        return initialEventIdea;
+    }
+
+    public void setInitialEventIdea(Idea initialEventIdea) {
+        this.initialEventIdea = initialEventIdea;
+    }
+
+    public Idea getCurrentInputIdea() {
+        return currentInputIdea;
+    }
+
+    public void setCurrentInputIdea(Idea currentInputIdea) {
+        this.currentInputIdea = currentInputIdea;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getInputMemoryName() {
+        return inputMemoryName;
+    }
+
+    public void setInputMemoryName(String inputMemoryName) {
+        this.inputMemoryName = inputMemoryName;
+    }
+
+    public String getOutputMemoryName() {
+        return outputMemoryName;
+    }
+
+    public void setOutputMemoryName(String outputMemoryName) {
+        this.outputMemoryName = outputMemoryName;
+    }
+
+    public void setEventCategoryName(String eventCategoryName) {
+        this.eventCategoryName = eventCategoryName;
+    }
+}
--- src/main/java/CSTEpisodicMemory/event/MoveEventTracker.java
+++ src/main/java/CSTEpisodicMemory/event/MoveEventTracker.java
@@ -1,93 +1,35 @@
 package CSTEpisodicMemory.event;
 
+import CSTEpisodicMemory.entity.EventTracker;
 import CSTEpisodicMemory.util.Vector2D;
-import br.unicamp.cst.core.entities.Codelet;
 import br.unicamp.cst.core.entities.Memory;
-import br.unicamp.cst.core.entities.MemoryObject;
 import br.unicamp.cst.representation.idea.Idea;
 
 import java.util.*;
 import java.util.List;
 
-import static CSTEpisodicMemory.util.IdeaPrinter.fullPrint;
+public class MoveEventTracker extends EventTracker {
 
-public class MoveEventTracker extends Codelet {
-
-    private Memory innerSenseMO;
-    private Memory eventsMO;
     private boolean debug = false;
-    private int bufferSize = 2;
-    private int bufferStepSize = 4;
-    private List<Idea> previousSelfIdea = new LinkedList<Idea>();
-    private Idea firstSelfIdea;
-    private Idea lastSelfIdea;
-    private Idea currentInnerSense;
-    private int count = 1;
 
     public MoveEventTracker(){
+        super("INNER", "EVENTS");
         this.name = "MoveEventTracker";
+        this.setEventCategoryName("Move");
+        this.setBufferSize(2);
+        this.setBufferStepSize(4);
     }
 
     public MoveEventTracker(boolean debug){
+        super("INNER", "EVENTS");
         this.name = "MoveEventTracker";
         this.debug = debug;
+        this.setEventCategoryName("Move");
+        this.setBufferSize(2);
+        this.setBufferStepSize(4);
     }
 
     @Override
-    public void accessMemoryObjects() {
-        this.innerSenseMO=(MemoryObject)this.getInput("INNER");
-        this.currentInnerSense = (Idea) innerSenseMO.getI();
-        this.eventsMO=(MemoryObject)this.getOutput("EVENTS");
-    }
-
-    @Override
-    public void calculateActivation() {
-
-    }
-
-    @Override
-    public void proc() {
-        //Initialize event track buffer
-        if (previousSelfIdea.size() == 0) {
-            previousSelfIdea.add(currentInnerSense.clone());
-        } else {
-            if (previousSelfIdea.size() < this.bufferSize) {
-                if (((int)currentInnerSense.get("Step").getValue())
-                        - ((int)previousSelfIdea.get(previousSelfIdea.size()-1).get("Step").getValue())
-                        >= bufferStepSize) {
-                    previousSelfIdea.add(currentInnerSense.clone());
-                }
-            } else {
-                if (((int)currentInnerSense.get("Step").getValue())
-                        - ((int)previousSelfIdea.get(previousSelfIdea.size()-1).get("Step").getValue())
-                        >= bufferStepSize) {
-                    //Check if current state is coherent with previous states and event category
-                    boolean check = belongsToEvent(previousSelfIdea, currentInnerSense);
-                    if (debug) System.out.println(check);
-                    if (check) {
-                        Idea drop = previousSelfIdea.remove(0);
-                        //Copies start of the event
-                        if (firstSelfIdea == null) this.firstSelfIdea = drop.clone();
-                        previousSelfIdea.add(currentInnerSense.clone());
-                    } else {
-                        if (firstSelfIdea != null) {
-                            Idea event = constructEventIdea(previousSelfIdea.get(1));
-                            previousSelfIdea.clear();
-                            previousSelfIdea.add(currentInnerSense.clone());
-                            firstSelfIdea = null;
-                            Idea eventsIdea = (Idea) eventsMO.getI();
-                            eventsIdea.add(event);
-                            if (debug) System.out.println("--------------\n" + fullPrint(eventsIdea));
-                        } else {
-                            previousSelfIdea.remove(0);
-                            previousSelfIdea.add(currentInnerSense.clone());
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     public boolean belongsToEvent(List<Idea> previous, Idea current){
         Idea a = previous.get(0);
         Idea b = previous.get(1);
@@ -108,31 +50,12 @@ public class MoveEventTracker extends Codelet {
         return false;
     }
 
-    private Idea constructEventIdea(Idea lastSelf){
-        Idea eventIdea = new Idea("Event" + count++, "Move", "Episode", 1);
-        Idea time1 = new Idea("", 1, "TimeStep", 1);
-        Idea time2 = new Idea("", 2, "TimeStep", 1);
-        time1.add(extractRelevant(firstSelfIdea));
-        time2.add(extractRelevant(lastSelf));
-        eventIdea.add(time1);
-        eventIdea.add(time2);
-        return eventIdea;
-    }
-
-    private Idea extractRelevant(Idea i){
+    @Override
+    public Idea extractRelevant(Idea i){
         Idea self = new Idea("Self", null, "AbstractObject", 1);
         self.add(i.get("Position").clone());
         self.add(i.get("Step").clone());
         return self;
     }
-
-    public int getBufferSize() {
-        return bufferSize;
-    }
-
-    public void setBufferSize(int bufferSize) {
-        if (bufferSize > 0) this.bufferSize = bufferSize;
-    }
-
 
 }
